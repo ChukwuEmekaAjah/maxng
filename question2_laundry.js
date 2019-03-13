7146bde861e189eba736501d33cb42ccaca4ea72
Ajah Chukwuemeka <talk2ajah@gmail.com>
2019-03-13 16:57:40 +0100

    update on question2 with comments and use of ES5

--- question2_laundry.js
+++ question2_laundry.js
@@ -8,7 +8,7 @@
 
 
 function arrange_socks(socks_pile, socks_container){
-	for(var i = 0; i < socks_pile.length; i++){
+	for(let i = 0; i < socks_pile.length; i++){
 		if(socks_container.hasOwnProperty(socks_pile[i])){
 			socks_container[socks_pile[i]] +=1;
 		}
@@ -21,16 +21,17 @@ function arrange_socks(socks_pile, socks_container){
 
 
 function get_pairs(number_of_washes,clean_socks,dirty_socks){
-	var clean_pairs = 0;
-	var remaining_colors = [];
-	for(var color in clean_socks){
+	let clean_pairs = 0;
+	let remaining_colors = [];
+	for(let color in clean_socks){
 		clean_pairs += Math.floor(clean_socks[color]/2);
 		clean_socks[color] = clean_socks[color] % 2;
 		if(clean_socks[color] == 1){
 			remaining_colors.push(color);
 		}
 	}
-	for(var color in dirty_socks){
+	// we give a higher priority to sock colors in remaining_colors array
+	for(let color in dirty_socks){
 		if(number_of_washes == 0){
 			break;
 		}
@@ -41,12 +42,13 @@ function get_pairs(number_of_washes,clean_socks,dirty_socks){
 			clean_pairs += 1;
 		}
 	}
-	for(var color in dirty_socks){
+	// we now check for other socks in the dirty_socks array
+	for(let color in dirty_socks){
 		if(number_of_washes <= 0){
 			break;
 		}
 		if(dirty_socks[color] > 1){
-			var possible_pairs = Math.floor((dirty_socks[color])/2);
+			let possible_pairs = Math.floor((dirty_socks[color])/2);
 			while(possible_pairs > 0 && number_of_washes >= 2){
 				number_of_washes -= 2;
 				possible_pairs -= 1;
@@ -57,18 +59,13 @@ function get_pairs(number_of_washes,clean_socks,dirty_socks){
 	return clean_pairs;
 }
 
-function get_max_number_of_socks_pair(noOfWashes, cleanPile, dirtyPile){
-	var clean_socks = arrange_socks(cleanPile,{});
-	var dirty_socks = arrange_socks(dirtyPile,{})
-	var max_pairs = get_pairs(noOfWashes,clean_socks,dirty_socks)
-	return max_pairs
-}
+
 
 function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
   	// Your solution should go here.
-  	var clean_socks = arrange_socks(cleanPile,{});
-	var dirty_socks = arrange_socks(dirtyPile,{})
-	var max_pairs = get_pairs(noOfWashes,clean_socks,dirty_socks)
+  	let clean_socks = arrange_socks(cleanPile,{});
+	let dirty_socks = arrange_socks(dirtyPile,{})
+	let max_pairs = get_pairs(noOfWashes,clean_socks,dirty_socks)
 	return max_pairs
 }
 
