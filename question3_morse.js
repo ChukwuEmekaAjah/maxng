d1fb83bc392b29cb945356698af3f902d5770cc6
Ajah Chukwuemeka <talk2ajah@gmail.com>
2019-03-13 17:01:44 +0100

    update on file to sooth es5 standards

--- question3_morse.js
+++ question3_morse.js
@@ -64,22 +64,22 @@ Object.freeze(MORSE_CODE);
  * @param {string} morseCode The string to decode.
  */
 function get_words(code){
-  var word_spacing = /\s{3}/g;
+  let word_spacing = /\s{3}/g;
   return code.split(word_spacing);
 }
 
-function get_characters(words_array){
-  var character_spacing = /\s{1}/g;
-  var words_array = words_array.map(function(word){
+function get_characters(words_array_in_morse){
+  let character_spacing = /\s{1}/g;
+  let words_array = words_array_in_morse.map(function(word){
     return word.split(character_spacing);
   })
   return words_array
 }
 
 function get_corresponding_word_characters(words_array,code){
-  var words_in_characters_array = words_array.map(function(word_array){
-    var word_characters_array = []
-    for(var i = 0; i < word_array.length; i++){
+  let words_in_characters_array = words_array.map(function(word_array){
+    let word_characters_array = []
+    for(let i = 0; i < word_array.length; i++){
       if(code[word_array[i]] !== undefined){
         word_characters_array.push(code[word_array[i]]);
       }
@@ -94,14 +94,14 @@ function get_corresponding_word_characters(words_array,code){
 
 
 function get_words_in_alphabet(words_characters_array){
-  var words = words_characters_array.map(function(word_characters_array){
+  let words = words_characters_array.map(function(word_characters_array){
     return word_characters_array.join('');
   })
   return words;
 }
 
 function  get_sentence(words_array){
-  var sentence = words_array.join(' ');
+  let sentence = words_array.join(' ');
   return sentence;
 }
 
@@ -110,11 +110,11 @@ function decodeMorse(morseCode) {
   if(!morseCode){
     return '';
   }
-  var cleaned_code = morseCode.trim()
-  var words_in_morse = get_words(cleaned_code);
-  var characters_in_morse = get_characters(words_in_morse);
-  var characters_in_alphabets = get_corresponding_word_characters(characters_in_morse, MORSE_CODE);
-  var words_in_alphabets = get_words_in_alphabet(characters_in_alphabets);
+  let cleaned_code = morseCode.trim()
+  let words_in_morse = get_words(cleaned_code);
+  let characters_in_morse = get_characters(words_in_morse);
+  let characters_in_alphabets = get_corresponding_word_characters(characters_in_morse, MORSE_CODE);
+  let words_in_alphabets = get_words_in_alphabet(characters_in_alphabets);
   return get_sentence(words_in_alphabets);
 }
 
